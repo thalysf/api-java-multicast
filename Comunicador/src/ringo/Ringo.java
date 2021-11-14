4cb0714f70803ee1bd6f4609fdd88b5aaeb5dbd7
thalysf <thalysfcandido@gmail.com>
2021-11-13 22:39:24 -0300

    Finalizando recomendações

--- Comunicador/src/ringo/Ringo.java
+++ Comunicador/src/ringo/Ringo.java
@@ -7,41 +7,41 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 
-public class Ringo extends Thread{
+public class Ringo extends Thread {
 
     Double menorDistancia = 0D;
     Double distancia = 0D;
     String nomeUsuarioMenorDistancia;
 
     List<Usuario> usuariosExistentes;
-    
-    
+
     public void recomendaParaUsuarios() {
         usuariosExistentes = recuperaUsuarios();
-        System.out.println("AQUI");
-        for(int i =0; i< usuariosExistentes.size(); i++)
-        {
-            System.out.println("------------------ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa ---------------");
-            System.out.println(calcularRecomendacoes(usuariosExistentes.remove(i)));
-            System.out.println("------------------ AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa ---------------");
+        for (int i = 0; i < usuariosExistentes.size(); i++) {
+            try {
+                System.out.println("------------------ Séries recomendadas para " + usuariosExistentes.get(i).getNome() + " ---------------");
+                System.out.println(calcularRecomendacoes(usuariosExistentes.remove(i)));
+                System.out.println("------------------ Séries recomendadas para " + usuariosExistentes.get(i).getNome() + " ---------------");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("=)");
+            }
         }
     }
-    
+
     // Calcula recomendações (função principal - chame ela)
     public List<String> calcularRecomendacoes(Usuario usuario) {
         // Usuário com menor distância ao que está sendo comparado:
         nomeUsuarioMenorDistancia = compararNotas(usuario);
 
         // Pegar recomendações
-        return  recuperaRecomendacoes(nomeUsuarioMenorDistancia, usuario.getNome());
+        return recuperaRecomendacoes(nomeUsuarioMenorDistancia, usuario.getNome());
     }
 
     // Recuperar recomendações
     public List<String> recuperaRecomendacoes(String usuarioMenorDistancia, String usuarioComparado) {
         List<String> seriesBemAvaliadas = new ArrayList<>();
-        List<String> seriesNaoVistas = new ArrayList<>();
+        List<String> seriesNaoVistasERecomendadas = new ArrayList<>();
         // Recuperando séries bem avaliadas do usuário de menor distância
         for (Avaliacao avaliacao : BdAvaliacoes.avaliacoes) {
             if (avaliacao.getUsuario().getNome().equalsIgnoreCase(usuarioMenorDistancia) && avaliacao.getNota() >= 2) {
@@ -51,16 +51,14 @@ public class Ringo extends Thread{
 
         // Recuperando séries não vistas pelo usuário comparado
         for (Avaliacao avaliacao : BdAvaliacoes.avaliacoes) {
-            if (avaliacao.getUsuario().getNome().equalsIgnoreCase(usuarioComparado) && avaliacao.getNota() == 0) {
-                seriesNaoVistas.add(avaliacao.getSerie());
+            if (avaliacao.getUsuario().getNome().equalsIgnoreCase(usuarioComparado)
+                    && avaliacao.getNota() == 0
+                    && seriesBemAvaliadas.contains(avaliacao.getSerie())) {
+                seriesNaoVistasERecomendadas.add(avaliacao.getSerie());
             }
         }
 
-        // Comparando bem avaliadas com não vistas
-//        List<String> interseccao = seriesBemAvaliadas.stream().filter(item1 -> {
-//            return seriesNaoVistas.stream().filter(item2 -> new Integer(item2).equals(item1)).findAny().isPresent();
-//        }).collect(Collectors.toList());
-        return seriesBemAvaliadas;
+        return seriesNaoVistasERecomendadas;
     }
 
     public String compararNotas(Usuario usuario) {
@@ -106,15 +104,17 @@ public class Ringo extends Thread{
         }
         return usuarios;
     }
-    
+
     @Override
-    public void run()
-    {
+    public void run() {
         try {
             System.out.println("Iniciando recomendações...");
-            Thread.sleep(2000);
+            Thread.sleep(1000);
             System.out.println("Recomendando séries:");
-            recomendaParaUsuarios();
+            while (true) {
+                recomendaParaUsuarios();
+                Thread.sleep(180000); // a cada 3 minutos as recomendações de séries são feitas
+            }
         } catch (InterruptedException ex) {
             Logger.getLogger(Ringo.class.getName()).log(Level.SEVERE, null, ex);
         }
